635d29a0c2791038e41c82582242cfe1d1f410fd
Dremaer <abdulkhamid-1@mail.ru>
2023-11-29 10:14:07 +0900

    3

--- app/src/main/java/com/example/mydiaryapplication/MainActivity.kt
+++ app/src/main/java/com/example/mydiaryapplication/MainActivity.kt
@@ -2,17 +2,30 @@ package com.example.mydiaryapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mydiaryapplication.*
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.example.mydiaryapplication.databinding.ActivityMainBinding
+import com.example.mydiaryapplication.Home
+import com.example.mydiaryapplication.Schedule
+import com.example.mydiaryapplication.Todolist
+import com.example.mydiaryapplication.Diary
+import com.example.mydiaryapplication.TimetableFragment
+import com.example.mydiaryapplication.GoalsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         replaceFragment(Home())
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
 
         binding.bottomNavigationView.setOnItemSelectedListener {
 
@@ -21,21 +34,22 @@ class MainActivity : AppCompatActivity() {
                 R.id.schedule -> replaceFragment(Schedule())
                 R.id.list -> replaceFragment(Todolist())
                 R.id.diary -> replaceFragment(Diary())
-                R.id.timetable -> replaceFragment(TimetableFragment())
+                R.id.time -> replaceFragment(TimetableFragment())
+                R.id.goals -> replaceFragment(GoalsFragment())
                 else -> {
-                    
+
                 }
             }
             true
         }
-
+        replaceFragment(Home())
 
     }
 
     private fun replaceFragment(fragment: Fragment) {
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout, fragment)
-        fragmentTransaction.commit()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame_layout, fragment)
+            .commit()
     }
+
 }
